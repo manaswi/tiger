f47ce21e0dfcfccb9e40c612f4453c435319accf
Manaswi  Sharma <montropistic@gmail.com>
2013-01-09 20:27:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiger::Application.config.secret_token = '4258dc8c040344ee6f71f76267522bcbcb4aca3a70af2312c23aebd24d0149c9cb789f7629af21108d31bf235b20a93b1d2a98e4bfdeb11155019152cabfa006'
